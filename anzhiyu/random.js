ab88ce84f194c249843c16a29cb9493a5fc41279
oyzj20000509 <oyzj20000509@gmail.com>
2025-09-18 21:44:09 +0800

    Site updated: 2025-09-18 21:44:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/17/测试/","2025/09/17/测试测试/","2025/09/17/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/09/17/测试/","2025/09/17/这是一篇新的博文/","2025/09/17/阿凡达1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
